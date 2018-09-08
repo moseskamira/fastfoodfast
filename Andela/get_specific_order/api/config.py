53c7b203352a1c2c42897c196de8407d273cb4ce
moses29 <moses.african@gmail.com>
2018-09-08 08:40:14 +0300

    API Endpoints for the App

--- Andela/get_specific_order/api/config.py
+++
@@ -1,9 +0,0 @@
-"""
-File Containing Global Server Settings
-"""
-
-HOST = "0.0.0.0"
-PORT = "5000"
-DEBUG = True
-TESTING = True
-ENVIRONMENT = "development"
--- Andela/get_specific_order/run.py
+++
@@ -1,6 +0,0 @@
-"""
-Module For Running The Application
-"""
-from api import APP
-if __name__ == '__main__':
-    APP.run(debug=True)
