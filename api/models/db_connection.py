3c08243177424d27a2862f01a9d66354b52729b9
moses29 <moses.african@gmail.com>
2018-10-04 20:42:31 +0300

    save changes

--- api/models/db_connection.py
+++ api/models/db_connection.py
@@ -15,7 +15,7 @@ class DBAccess(object):
         Method For Establishing Connection To Databse
         """
         connection = psycopg2.connect(
-            "dbname='FastFoodFast' user='postgres' host='localhost' password='moses12' port='5433'"
+            "dbname='d4or467mumdvn' user='bzjrdbxajezifp' host='ec2-23-23-80-20.compute-1.amazonaws.com' password='004afaa0dfedda5ec4bfb867d47e98a7417b6913c16f91c8019b60cf67bb168c' port='5432'"
         )
         return connection     
 
