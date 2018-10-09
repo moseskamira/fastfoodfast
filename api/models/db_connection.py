eca85e1287fb13b9240695dbe1205584e7c4bec2
moses29 <moses.african@gmail.com>
2018-10-09 03:31:04 +0300

    save new changes

--- api/models/db_connection.py
+++ api/models/db_connection.py
@@ -17,7 +17,7 @@ class DBAccess(object):
         if os.getenv('db')== 'heroku':
             connection = psycopg2.connect(
             # "dbname='d4or467mumdvnf' user='bzjrdbxajezifp' host='ec2-23-23-80-20.compute-1.amazonaws.com' password='004afaa0dfedda5ec4bfb867d47e98a7417b6913c16f91c8019b60cf67bb168c' port='5432'"
-            "host='ec2-23-23-80-20.compute-1.amazonaws.com' database='d4or467mumdvnf' user='bzjrdbxajezifp' port='5432' password='004afaa0dfedda5ec4bfb867d47e98a7417b6913c16f91c8019b60cf67bb168c'"
+            "host='ec2-23-23-80-20.compute-1.amazonaws.com' dbname='d4or467mumdvnf' user='bzjrdbxajezifp' port='5432' password='004afaa0dfedda5ec4bfb867d47e98a7417b6913c16f91c8019b60cf67bb168c'"
         )
             return connection
         else:
